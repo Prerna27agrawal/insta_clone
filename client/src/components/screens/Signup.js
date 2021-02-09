c0b26783a07cfdabb93be903c3b6f7c7319881c3
Prerna27agrawal <agrawalprerna1999@gmail.com>
2021-02-09 15:02:25 +0530

    updating profile

--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {Link,useHistory} from 'react-router-dom';
 import M from 'materialize-css';
 
@@ -7,16 +7,40 @@ const SignUp =()=>{
   const[name,setName]= useState("");
   const[password,setPassword]= useState("");
   const[email,setEmail]= useState("");
+  const[image,setImage]= useState("");
+  const[url,setUrl]= useState(undefined);
 
-  // for sending the post request to the server host
-   const PostData=()=>{
+  useEffect(()=>{
+      if(url){
+        uploadFields();
+      }
+  },[url])
+
+  const uploadPic =()=>{
+       const data = new FormData();
+      data.append("file",image);
+      data.append("upload_preset","insta-clone");
+      data.append("cloud_name","dhr7wlz2k");
+      fetch("	https://api.cloudinary.com/v1_1/dhr7wlz2k/image/upload",{
+          method:"post",
+          body:data
+      }).then(res=>res.json()).then(data=>{
+          setUrl(data.url);
+      }).catch(err=>{
+          console.log(err);
+      });
+  }
+
+
+  const uploadFields = ()=>{
+    
     if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
       return  M.toast({html:"Please fill valid email",classes:"#c62828 red darken-3"});
     }
-//  for sending data from 5000 host that is the serve side to localhost 3000 on which react is running
-// we can either use CORS from npm lib or
-  // we can add "proxy":"http://localhost:5000", in package.json file
-  // and now we can send the relative path and it will automatically send it to server side with different ports  
+        //  for sending data from 5000 host that is the serve side to localhost 3000 on which react is running
+         // we can either use CORS from npm lib or
+         // we can add "proxy":"http://localhost:5000", in package.json file
+           // and now we can send the relative path and it will automatically send it to server side with different ports  
     fetch("/signup",{
       method:"post",
       headers:{
@@ -25,7 +49,8 @@ const SignUp =()=>{
       body:JSON.stringify({
         name:name,
         password:password,
-        email:email
+        email:email,
+        pic:url
       })
     }).then(res=>res.json()).then(data=>{
       if(data.error){
@@ -38,7 +63,22 @@ const SignUp =()=>{
     }).catch(err=>{
       console.log(err);
     })
+  }
+  
+
+      // for sending the post request to the server host
+   const PostData=()=>{
+
+    if(image){
+      uploadPic();
+    }
+    else{
+      uploadFields();
+    }
+
    }
+
+
 
  return (
      <div className="mycard">
@@ -62,6 +102,15 @@ const SignUp =()=>{
             value={password}
             onChange={(e)=> setPassword(e.target.value)}
             />
+            <div className="file-field input-field">
+                 <div className="btn waves-effect waves-light #00796b teal darken-2">
+                     <span>Profile Pic</span>
+                     <input type="file" onChange={(e)=> setImage(e.target.files[0])} />
+                 </div>
+                 <div className="file-path-wrapper" >
+                     <input className="file-path validate" type="text" />
+                 </div>
+             </div>
             <button onClick={()=> PostData()} className="btn waves-effect waves-light #00796b teal darken-2">
                SignUp
             </button>
